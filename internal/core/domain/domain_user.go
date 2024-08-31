1e15d61e01c0d0eb21f2d15f8af1cb709da5e56a
fullgukbap <2hanjunbum6@gmail.com>
2024-08-31 20:22:12 +0900

    add: 사용자(user) 도메인 추가 완료

---
+++ internal/core/domain/domain_user.go
@@ -0,0 +1,19 @@
+package domain
+
+type UserRoleType string
+
+const (
+	Guest   UserRoleType = "GUEST"
+	Manager UserRoleType = "MANAGER"
+	ADMIN   UserRoleType = "ADMIN"
+)
+
+type User struct {
+	ID        int
+	LoginID   int
+	Password  string
+	Email     *string
+	Role      UserRoleType
+	CreatedAt string
+	UpdatedAt string
+}
