9de603af791cb59bcb6857609fac9dcb07c69912
fullgukbap <2hanjunbum6@gmail.com>
2024-09-02 09:20:15 +0900

    feat: domain error 추가

--- domain/errors.go
+++ domain/errors.go
@@ -3,8 +3,15 @@ package domain
 import "toktok-backend-v2/pkg/errors"
 
 var (
-	ErrInternalServerError = errors.New("internal server errors")
-	ErrNotFound            = errors.New("item is not found")
-	ErrConflict            = errors.New("item is conflict")
-	ErrBadParam            = errors.New("given param is not valid")
+	ErrInternalServerError  = errors.New("internal server errors")          // 500 Internal Server Error
+	ErrNotFound             = errors.New("item is not found")               // 404 Not Found
+	ErrConflict             = errors.New("item is conflict")                // 409 Conflict
+	ErrBadParam             = errors.New("given param is not valid")        // 400 Bad Request
+	ErrDatabaseOperation    = errors.New("database operation failed")       // 500 Internal Server Error
+	ErrInvalidTransaction   = errors.New("invalid transaction")             // 400 Bad Request
+	ErrForeignKeyViolation  = errors.New("foreign key constraint violated") // 409 Conflict
+	ErrUniqueKeyViolation   = errors.New("unique key constraint violated")  // 409 Conflict
+	ErrCheckConstraint      = errors.New("check constraint violated")       // 422 Unprocessable Entity
+	ErrInvalidQuery         = errors.New("invalid query")                   // 400 Bad Request
+	ErrUnsupportedOperation = errors.New("unsupported operation")           // 501 Not Implemented
 )
